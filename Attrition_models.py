9aea715b34fa3023622dcd94b356cdc8a4a3b3b6
Tamara Williams <tamtwill1@gmail.com>
2019-02-12 16:44:09 -0800

    Updating model code
    Adding some placeholder ideas I want to work on in the final model code

--- Attrition_models.py
+++ Attrition_models.py
@@ -19,11 +19,13 @@ from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import confusion_matrix, f1_score
 
 
-rnd_st = np.random.RandomState(31)
+RND_ST = np.random.RandomState(31)
 
 
 datapath = '~/gitRepo/Predicting_Employee_Attrition/data/'
 outpath = '~/gitRepo/Predicting_Employee_Attrition/output/'
+N_FOLDS = 10
+RND_ST = 31
 
 df = pd.read_csv(datapath+'processed_train.csv', delimiter=',', encoding='utf-8')
 
@@ -31,22 +33,37 @@ train = df.copy(deep=True)
 train = train.drop(columns=['has_quit'])
 target = df['has_quit']
 
+## from https://www.programcreek.com/python/example/91149/sklearn.model_selection.StratifiedShuffleSplit
+## 
+# def validate(data, labels):   
+#     '''
+#     Ten-fold cross-validation with stratified sampling.
+#     '''
+#     accuracy_scores = []
+#     precision_scores = []
+#     recall_scores = []
+#     f1_scores = []
 
-# split data into train/test sets
-# Import the train_test_split method
+#     stsp = StratifiedShuffleSplit(n_splits=N_FOLDS)
+#     for train_index, test_index in stsp.split(data, labels):
+#         x_train, x_test = data[train_index], data[test_index]
+#         y_train, y_test = labels[train_index], labels[test_index]
+#         clf.fit(x_train, y_train)
+#         y_pred = clf.predict(x_test)
+#         accuracy_scores.append(accuracy_score(y_test, y_pred))
+#         precision_scores.append(precision_score(y_test, y_pred))
+#         recall_scores.append(recall_score(y_test, y_pred))
+#         f1_scores.append(f1_score(y_test, y_pred))
+
+#     print('Accuracy', np.mean(accuracy_scores))
+#     print('Precision', np.mean(precision_scores))
+#     print('Recall', np.mean(recall_scores))
+#     print('F1-measure', np.mean(f1_scores)) 
 
 
-# Split data into train and test sets as well as for validation and testing
-train, test, target_train, target_val = train_test_split(train, target, train_size= 0.70, random_state=rnd_st)
-#train, test, target_train, target_val = StratifiedShuffleSplit(attrition_final, target, random_state=0)
 
-log_reg = LogisticRegression()
-log_reg.fit(train, target_train)
-train_predict = log_reg.predict(train)
-test_predict = log_reg.predict(test)
-y_prob = log_reg.predict(train)
-y_pred = np.where(y_prob > 0.5, 1, 0)
-train_test_error(train_predict , test_predict)
 
-oversampler=SMOTE(random_state=rnd_st)
-smote_train, smote_target = oversampler.fit_sample(train,target)
+
+# test whether over sampling helps
+# oversampler=SMOTE(random_state=rnd_st)
+# smote_train, smote_target = oversampler.fit_sample(train,target)
